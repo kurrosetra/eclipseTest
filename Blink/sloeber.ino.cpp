136bb6398f8b1e8fab2b9874ed8deadd47b28ea9
kurro <kurrosetra@gmail.com>
2018-02-20 14:16:44 +0700

    test change commit

--- Blink/sloeber.ino.cpp
+++ Blink/sloeber.ino.cpp
@@ -2,9 +2,10 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2018-02-20 14:07:59
+//This file has been generated on 2018-02-20 14:16:14
 
 #include "Arduino.h"
+#include <Arduino.h>
 
 void setup() ;
 void loop() ;
